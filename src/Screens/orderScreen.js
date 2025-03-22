51ef5f69c2465e65d2671ca6e408336b9574c8a7
eranmazuz <mazuzeran@gmail.com>
2025-03-22 23:43:01 +0200

    - Finish order screen,

--- src/Screens/orderScreen.js
+++ src/Screens/orderScreen.js
@@ -3,18 +3,20 @@ import OrderList from "../Components/orderList";
 import {Button, Stack, Typography} from "@mui/material";
 import CAPTIONS from "../Constants/captions";
 import RequiredInput from "../Components/requiredInput";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {cartActions} from "../Store/cartSlice";
 import ERRORS from "../Constants/errors";
 import {ordersActions} from "../Store/ordersSlice";
+import axios from "axios";
 
 const OrderScreen = () => {
     const [fullName, setFullName] = useState('')
     const [address, setAddress] = useState('')
     const [email, setEmail] = useState('')
+    const products = useSelector(state => state.cart.items)
 
     const dispatch = useDispatch();
-    const handleFinishOrder = useCallback(() =>  {
+    const handleFinishOrder = useCallback(async () =>  {
         dispatch(cartActions.clearErrors())
         const errors = {}
         if(!fullName.trim()) {
@@ -27,12 +29,26 @@ const OrderScreen = () => {
             errors.email = ERRORS.REQUIRED
         }
         if(Object.keys(errors).length === 0) {
+            const cart = []
+            Object.entries(products).forEach(([category, items]) => {
+                Object.entries(items).forEach(([name, amount]) => {
+                    cart.push({
+                        category,
+                        name,
+                        amount
+                    })
+                })
+            })
+            const orderData = { fullName, address, email, cart };
+            console.log(orderData)
+            await axios.post(process.env.REACT_APP_POST_ORDER_URL, orderData)
+            alert('Order placed!');
 
         } else {
             dispatch(ordersActions.setErrors(errors))
         }
 
-    }, [fullName, address, email, dispatch])
+    }, [fullName, address, email, products, dispatch])
     return (
         <Stack spacing={2} padding={2}>
             <Typography variant="h4">{CAPTIONS.ORDER_SUMMERY}</Typography>
